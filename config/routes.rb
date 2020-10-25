57a7e6e6f3643283eeeca431c254ab405803b517
jomariepolanco <jomariepolanco@gmail.com>
2020-10-25 17:41:00 -0400

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'about', to: 'static#about'
 end
+
+#the HTTP verb - GET
+#the path 'about' represent path in URL bar
+#controller action 'static#about' tells routing system that this route should be passed through static controller's about action
+#action = controller method
